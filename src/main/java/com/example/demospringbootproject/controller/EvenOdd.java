8cfffb7dae848b56fabb15b7b7202534c802f023
tannu-8 <tanubhushan08@gmail.com>
2024-02-04 10:17:13 +0530

    Spring Assignment1

---
+++ src/main/java/com/example/demospringbootproject/controller/EvenOdd.java
@@ -0,0 +1,18 @@
+package com.example.demospringbootproject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EvenOdd {
+    @RequestMapping("/EvenOddNumber")
+    public void evenOdd() {
+        int n = 4;
+        if (n % 2 == 0) {
+            System.out.println(n + " Number is even");
+        } else {
+            System.out.println(n + " Number is odd");
+        }
+    }
+}
+
---
+++ src/main/java/com/example/demospringbootproject/controller/PrimeNumber.java
@@ -0,0 +1,24 @@
+package com.example.demospringbootproject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrimeNumber {
+    @RequestMapping("/PrimeNumber")
+    public boolean Prime(){
+        int n=13;
+        if (n==0 || n==1){
+            System.out.println("0 and 1 are not prime ");
+            return false;
+        }
+        for (int i=2;i<n;i++){
+            if(i%n==0){
+                System.out.println(" The number "+n+" is not prime");
+                return false;
+            }
+        }
+        System.out.println(" The number " +n+" is prime");
+        return true;
+    }
+}
