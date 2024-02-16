d68043062f769bb5ea63973bd88a0d612400b2e8
shub07hcst <shub6240@gmail.com>
2024-02-15 19:20:14 -0800

    Assignment of Postman

--- src/main/java/com/example/demospringbootproject/crud/EmployeeCrud.java
+++ src/main/java/com/example/demospringbootproject/crud/EmployeeCrud.java
@@ -1,9 +1,7 @@
 package com.example.demospringbootproject.crud;
 
 import com.example.demospringbootproject.model.Employee;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -11,25 +9,31 @@ import java.util.ArrayList;
 public class EmployeeCrud {
 
     ArrayList<Employee> employeeCrud=new ArrayList<>();
-    @RequestMapping("/add_emp")
+   // @RequestMapping("/add_emp")
+    @PostMapping("/add_emp")
     public String addEmployee(@RequestParam String name, @RequestParam int age, @RequestParam String department,@RequestParam String empId){
         Employee employee=new Employee(name,age,department,empId);
         employeeCrud.add(employee);
         return "Employee added successfully";
     }
 
-    @RequestMapping("/read_emp")
+   // @RequestMapping("/read_emp")
+    @GetMapping("/read_emp")
 
     public ArrayList<Employee> readEmployee(){
+
         return employeeCrud;
     }
-    @RequestMapping("/update_emp")
+
+  //  @RequestMapping("/update_emp")
+    @PutMapping("/update_emp")
     public String updateEmployee(@RequestParam String newDepartment,@RequestParam int index){
         Employee employee=employeeCrud.get(index);
         employee.setDepartment(newDepartment);
         return "Employee Department Update Successfully";
     }
-    @RequestMapping("/delete_emp")
+   // @RequestMapping("/delete_emp")
+    @DeleteMapping("/delete_emp")
     public String deleteEmployee(@RequestParam int index){
         employeeCrud.remove(index);
         return "Deleted successfully";
--- src/main/java/com/example/demospringbootproject/crud/StudentCrud.java
+++ src/main/java/com/example/demospringbootproject/crud/StudentCrud.java
@@ -1,9 +1,7 @@
 package com.example.demospringbootproject.crud;
 
 import com.example.demospringbootproject.model.Student;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -11,7 +9,8 @@ import java.util.ArrayList;
 public class StudentCrud {
 
      ArrayList<Student> studentCrud=new ArrayList<>();
-     @RequestMapping("/add_stud")
+   //  @RequestMapping("/add_stud")
+     @PostMapping("/add_stud")
      public String addStudent(@RequestParam String name, @RequestParam int rollNo, @RequestParam String section){
           Student student=new Student(name, rollNo, section);
           studentCrud.add(student);
@@ -19,19 +18,22 @@ public class StudentCrud {
 
      }
 
-     @RequestMapping("/read_stud")
+    // @RequestMapping("/read_stud")
+     @GetMapping("/read_stud")
      public ArrayList<Student> readStudent(){
           return studentCrud;
      }
 
-     @RequestMapping("/update_stud")
+    // @RequestMapping("/update_stud")
+     @PutMapping("/update_stud")
      public String updateStudent(@RequestParam String newName,@RequestParam int index){
           Student student=studentCrud.get(index);
           student.setName(newName);
           return" Student name update successfully";
      }
 
-     @RequestMapping("/delete_stud")
+    // @RequestMapping("/delete_stud")
+     @DeleteMapping("/delete_stud")
      public String deleteStudent(@RequestParam int index){
           studentCrud.remove(index);
           return "Deleted successfully";
