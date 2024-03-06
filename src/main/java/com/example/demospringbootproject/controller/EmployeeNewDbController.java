f43a355f8515da6bb0d3aeb8532c1211f9f1e734
tannu-8 <tanubhushan08@gmail.com>
2024-03-06 06:47:01 +0530

    CRUD with DB Of Employee

---
+++ src/main/java/com/example/demospringbootproject/controller/EmployeeNewDbController.java
@@ -0,0 +1,53 @@
+package com.example.demospringbootproject.controller;
+
+import com.example.demospringbootproject.entity.EmployeeNew;
+import com.example.demospringbootproject.service.EmployeeNewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class EmployeeNewDbController {
+    @Autowired
+    EmployeeNewService service;
+
+    @PostMapping("/add_emp")
+    public String add(@RequestBody EmployeeNew employee) {
+        return service.add(employee);
+    }
+
+    @GetMapping("/employees")
+    public List<EmployeeNew> getAllEmployee() {
+        return service.getAllEmployees();
+    }
+
+    @GetMapping("/employee/{id}")
+    public EmployeeNew getEmployeeById(@PathVariable Long id) {
+        return service.getEmployeeById(id);
+
+    }
+
+    @PutMapping("/update_employee_name/{name}/{id}")
+    public String updateEmployeeName(@PathVariable String name, @PathVariable long id) {
+        return service.updateName(name, id);
+
+    }
+
+    @PutMapping("/update_Age/{age}/{id}")
+    public String updateAge(@PathVariable int age, @PathVariable long id) {
+        return service.updateAge(age, id);
+    }
+
+    @PutMapping("/update_CityName/{cityName}/{id}")
+    public String updateCityName(@PathVariable String cityName, @PathVariable long id) {
+        return service.updateCity(cityName, id);
+    }
+
+    @DeleteMapping("/remove_Employee/{id}")
+    public String removeEmployee(@PathVariable long id) {
+        return service.removeEmployee(id);
+
+    }
+
+}
---
+++ src/main/java/com/example/demospringbootproject/repository/EmployeeNewRepository.java
@@ -0,0 +1,9 @@
+package com.example.demospringbootproject.repository;
+
+import com.example.demospringbootproject.entity.EmployeeNew;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeNewRepository extends JpaRepository<EmployeeNew,Long> {
+}
